310720e26bd8e86ba2422ce60c0b8d8981c43621
Rabin Ramarajan <100574003+Rabinramarajan@users.noreply.github.com>
2022-05-28 15:35:10 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+//Navbar section js
+
+
+const menuBtn = document.querySelector(".menu-btn");
+    const navbar = document.querySelector(".navbar");
+
+    menuBtn.addEventListener("click", () => {
+      menuBtn.classList.toggle("active");
+      navbar.classList.toggle("active");
+    });
+
+    var nav = document.getElementById('nav');
+    window.onscroll = function(){
+      if (window.pageYOffset >650){
+        nav.style.background = "rgba(1, 1, 1, 0.5)";
+      }
+      else{
+        nav.style.background = "transparent";
+      }
+    }
+
+    //Javacript for video slider navigation
+    const btns = document.querySelectorAll(".nav-btn");
+    const slides = document.querySelectorAll(".video-slide");
+    const contents = document.querySelectorAll(".content");
+
+    var sliderNav = function(manual){
+      btns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+
+      contents.forEach((content) => {
+        content.classList.remove("active");
+      });
+
+      btns[manual].classList.add("active");
+      slides[manual].classList.add("active");
+      contents[manual].classList.add("active");
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        sliderNav(i);
+      });
+    });
+
